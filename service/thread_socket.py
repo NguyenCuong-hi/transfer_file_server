d0fdf5819191696571655e2b89c2245b58455c77
CuongNv <nvc.nvcuong@gmail.com>
2024-09-18 16:30:23 +0700

    fix: send file csvcsv, txt

--- service/thread_socket.py
+++ service/thread_socket.py
@@ -18,21 +18,14 @@ class ListeningThread(QThread):
         try:
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_socket.bind((self.host, self.port))
+            # server_socket.connect(self.host, self.port)
             server_socket.listen(5)
-            # server_socket.setblocking(True)
             self.transfer_progress.emit(f"Listening on {self.host}:{self.port}...")
 
             while self.running:
                 try:
-                    # ready_to_read, _, _ = select.select([server_socket], [], [], 1)
-                    # if ready_to_read == True:
                     client_socket, client_address = server_socket.accept()
                     self.transfer_progress.emit(f"Connected to {client_address}")
-
-                    # client_thread = threading.Thread(target=self.handle_client,
-                    #                                  args=(client_socket, client_address))
-                    # client_thread.start()
-
                     self.handle_client(client_socket=client_socket, client_address=client_address)
                 except Exception as e:
                     self.transfer_progress.emit(f"Error: {str(e)}")
@@ -51,25 +44,15 @@ class ListeningThread(QThread):
     def handle_client(self, client_socket, client_address):
         file_name = client_socket.recv(100).decode()
         file_size = client_socket.recv(100).decode()
-        # if file_info == 'PING':
-        #     print(f"Ping received from {client_address}, no action taken.")
-        #     client_socket.close()
-        #     return
         try:
-            # file_name, file_size = file_info.split('|')
-            with open(file_name, 'w', encoding='utf-8') as f:
+            with open("./rec/" + file_name, 'w', encoding='utf-8') as f:
                 total_received = 0
-
-                while total_received < file_size:
+                while True:
                     data = client_socket.recv(1024).decode('utf-8')
-                    if not data:
-                        break
-                    self.transfer_progress.emit(f"Received from {client_address}: {data}")
                     f.write(data)
-                    total_received += len(data)
-                    print(f"Received data from{client_address}: {data}")
                     f.close()
                     client_socket.close()
+                
         except Exception as e:
             self.transfer_progress.emit(f"Error handling client {client_address}: {str(e)}")
             print(f"Error handling client {client_address}: {str(e)}")
